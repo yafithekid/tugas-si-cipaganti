7d16e38b4dfe5d6ff224fc9cfa11f651cb4a7970
Timothy Pratama <timothy.pratama@gmail.com>
2015-04-14 17:57:07 +0700

    Added Jadwal Seeder Generator (can be used for > 1 days)

--- database/seeds/PoolSeeder.php
+++ database/seeds/PoolSeeder.php
@@ -14,25 +14,25 @@ class PoolSeeder extends Seeder {
 	public function run()
 	{
 		DB::table('pool')->delete();
-
+        $id = 1;
         $kotas = array(
-            ['id' => 1, 'nama' => 'Bandung Trade Center (BTC)','kota_id' => 1],
-            ['id' => 2, 'nama' => 'Pasteur Point','kota_id' => 1],
-            ['id' => 3, 'nama' => 'Festival City Link (Citylink)','kota_id' => 1],
-            ['id' => 4, 'nama' => 'Miko Mall Kopo (Miko)','kota_id' => 1],
-            ['id' => 5, 'nama' => 'Dipati Ukur (DU)','kota_id' => 1],
-            ['id' => 6, 'nama' => 'Gatot Subroto 94 (Gatsu-94)','kota_id' => 1],
-            ['id' => 7, 'nama' => 'Cipaganti 75 (CPGT-75)','kota_id' => 1],
-            ['id' => 8, 'nama' => 'Gede Bage (GDBG)','kota_id' => 1],
+            ['id' => $id++, 'nama' => 'Bandung Trade Center (BTC)','kota_id' => 1],
+            ['id' => $id++, 'nama' => 'Pasteur Point','kota_id' => 1],
+            ['id' => $id++, 'nama' => 'Festival City Link (Citylink)','kota_id' => 1],
+            ['id' => $id++, 'nama' => 'Miko Mall Kopo (Miko)','kota_id' => 1],
+            ['id' => $id++, 'nama' => 'Dipati Ukur (DU)','kota_id' => 1],
+            ['id' => $id++, 'nama' => 'Gatot Subroto 94 (Gatsu-94)','kota_id' => 1],
+            ['id' => $id++, 'nama' => 'Cipaganti 75 (CPGT-75)','kota_id' => 1],
+            ['id' => $id++, 'nama' => 'Gede Bage (GDBG)','kota_id' => 1],
 
-            ['id' => 9, 'nama' => 'Pondok Indah','kota_id' => 2],
-            ['id' => 10, 'nama' => 'Mangga Dua Square (MG2)','kota_id' => 2],
-            ['id' => 11, 'nama' => 'Pancoran (PNCRN)','kota_id' => 2],
-            ['id' => 12, 'nama' => 'Dewi Sartika(DS)','kota_id' => 2],
-            ['id' => 13, 'nama' => 'Rawamangun (RWMGN)','kota_id' => 2],
-            ['id' => 14, 'nama' => 'Jatiwaringin (JTWRGN)','kota_id' => 2],
-            ['id' => 15, 'nama' => 'Sumenep Blora','kota_id' => 2],
-            ['id' => 16, 'nama' => 'Kemanggisan (KMGSN)','kota_id' => 2]
+            ['id' => $id++, 'nama' => 'Pondok Indah','kota_id' => 2],
+            ['id' => $id++, 'nama' => 'Mangga Dua Square (MG2)','kota_id' => 2],
+            ['id' => $id++, 'nama' => 'Pancoran (PNCRN)','kota_id' => 2],
+            ['id' => $id++, 'nama' => 'Dewi Sartika(DS)','kota_id' => 2],
+            ['id' => $id++, 'nama' => 'Rawamangun (RWMGN)','kota_id' => 2],
+            ['id' => $id++, 'nama' => 'Jatiwaringin (JTWRGN)','kota_id' => 2],
+            ['id' => $id++, 'nama' => 'Sumenep Blora','kota_id' => 2],
+            ['id' => $id++, 'nama' => 'Kemanggisan (KMGSN)','kota_id' => 2]
         );
 
         DB::table('pool')->insert($kotas);
